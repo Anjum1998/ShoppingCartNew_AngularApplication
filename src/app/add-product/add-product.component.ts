9b1caf90f365364c14a80dd51529901c96dc86c1
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-08 00:08:17 +0530

    api

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-product',
@@ -12,15 +13,29 @@ category=""
 description=""
 price=""
 
+constructor(private api:ApiService){}
 readValue=()=>
 {
   let data:any={"pname":this.pname,"image":this.image,"category":this.category,"description":this.description,"price":this.price}
   console.log(data)
-  this.category=""
-  this.description=""
-  this.image=""
-  this.pname=""
-  this.price
+  this.api.addProduct(data).subscribe(
+    (response:any)=>
+    {
+      console.log(response)
+      if (response.status=="success") {
+        alert("product added successfully")
+        this.category=""
+        this.description=""
+        this.image=""
+        this.pname=""
+        this.price=""
+      } else {
+        alert("something went wrong")
+        
+      }
+    }
+  )
+  
   
 }
 }
