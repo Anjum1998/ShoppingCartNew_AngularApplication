e699cfaba8f340da742fa87cf734191fd0566dda
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-07 21:32:22 +0530

    add

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class UserRegisterComponent {
 
+  name=""
+  address=""
+  phone=""
+  email=""
+  password=""
+  confirm=""
+  
+  readValue=()=>
+  {
+    let data:any={ "name":this.name,"address":this.address,"phone":this.phone,"email":this.email,"password":this.password
+  ,"confirm":this.confirm}
+  console.log(data)
+  if (this.password==this.confirm) {
+    alert("successfully added")
+    this.address=""
+    this.confirm=""
+    this.email=""
+    this.name=""
+    this.password=""
+    this.phone=""
+  } else {
+    alert("password does not match")
+  }
+  }
+
 }
